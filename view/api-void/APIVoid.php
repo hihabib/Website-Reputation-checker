27b11b559090c0faea2a7ad78355b58b9aaf2bd1
Habibul Islam <habibulislam6862@gmail.com>
2024-08-20 16:28:52 +0600

    updated

--- view/api-void/APIVoid.php
+++ view/api-void/APIVoid.php
@@ -341,10 +341,12 @@ new class {
                 border-radius: 8px;
                 padding: 10px
             }
+
             .reputation_domain_icon {
                 width: 15px;
                 margin-top: 8px;
             }
+
             div:has(>.reputation_domain_icon) {
                 display: flex;
                 gap: 10px;
@@ -352,30 +354,52 @@ new class {
             }
         </style>
         <div class="recent_reputation_checks">
-            <pre>
             <?php
-             print_r($result);
-            ?>
-                </pre>
-            <div class="reputation_site">
-                <div>
-                    <div class="reputation_domain_icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                            <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                            <path d="M320 464c8.8 0 16-7.2 16-16l0-288-80 0c-17.7 0-32-14.3-32-32l0-80L64 48c-8.8 0-16 7.2-16 16l0 384c0 8.8 7.2 16 16 16l256 0zM0 64C0 28.7 28.7 0 64 0L229.5 0c17 0 33.3 6.7 45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3L384 448c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64z"/>
-                        </svg>
+            foreach ($result as $website_data) :
+
+                ?>
+                <div class="reputation_site">
+                    <div>
+                        <div class="reputation_domain_icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                <path d="M320 464c8.8 0 16-7.2 16-16l0-288-80 0c-17.7 0-32-14.3-32-32l0-80L64 48c-8.8 0-16 7.2-16 16l0 384c0 8.8 7.2 16 16 16l256 0zM0 64C0 28.7 28.7 0 64 0L229.5 0c17 0 33.3 6.7 45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3L384 448c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64z"/>
+                            </svg>
+                        </div>
+                        <div class="reputation_domain_name"><a
+                                    href="https://reportscammedfunds.com/website-reputation-checker/?url=<?php echo $website_data->url; ?>"><?php echo $website_data->url; ?></a>
+                        </div>
                     </div>
-                    <div class="reputation_domain_name">just-fly.aero</div>
-                </div>
-                <div>
-                    <div class="reputation_domain_time">
-                        3 minutes ago
+                    <div>
+                        <div class="reputation_domain_time">
+                            <?php echo $this->time_ago($website_data->time); ?>
+                        </div>
                     </div>
                 </div>
-            </div>
+            <?php endforeach; ?>
         </div>
         <?php
         return ob_get_clean();
 
+    }
+
+    public function time_ago($datetime)
+    {
+        $currentTime = new DateTime();
+        $givenTime = new DateTime($datetime);
+        $interval = $currentTime->diff($givenTime);
+
+        if ($interval->y > 0) {
+            return $interval->y . ' year' . ($interval->y > 1 ? 's' : '') . ' ago';
+        } elseif ($interval->m > 0) {
+            return $interval->m . ' month' . ($interval->m > 1 ? 's' : '') . ' ago';
+        } elseif ($interval->d > 0) {
+            return $interval->d . ' day' . ($interval->d > 1 ? 's' : '') . ' ago';
+        } elseif ($interval->h > 0) {
+            return $interval->h . ' hour' . ($interval->h > 1 ? 's' : '') . ' ago';
+        } elseif ($interval->i > 0) {
+            return $interval->i . ' minute' . ($interval->i > 1 ? 's' : '') . ' ago';
+        } else {
+            return $interval->s . ' second' . ($interval->s > 1 ? 's' : '') . ' ago';
+        }
     }
 };
