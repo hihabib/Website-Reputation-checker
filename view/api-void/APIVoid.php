f8deb6ca421678adfa5320cc1c610ccd12d969d0
Habibul Islam <habibulislam6862@gmail.com>
2024-08-21 13:18:32 +0600

    updated

--- view/api-void/APIVoid.php
+++ view/api-void/APIVoid.php
@@ -208,7 +208,7 @@ new class {
             A free online domain risk score tool you can use to get reputation of a
             domain. If you're concerned about a domain, this tool can help you find out if the domain is malicious.
             Simply enter the domain name in the form below and press the button.</p>
-        <h5><a href="https://reportscammedfunds.com/recent-checks/">Check recent checks.</a></h5>
+        <h5 style="text-align:center; color: black"><a href="https://reportscammedfunds.com/recent-checks/">Check recent checks.</a></h5>
         <form id="api-void" action="<?php the_permalink(); ?>">
             <div>
                 <div>
